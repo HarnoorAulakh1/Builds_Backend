b50b901af73eb2e388aa5d716872ec5601d6b5bd
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2024-07-31 23:41:27 +0530

    port added to websockets 4

--- websocket/chat.js
+++ websocket/chat.js
@@ -4,7 +4,7 @@ import WebSocket from "ws";
 import messages from "../schema/messages.js";
 
 const app = express();
-const port =8080;
+const port =443;
 const httpServer = app.listen(port);
 export const wss = new WebSocketServer({ server: httpServer });
 
