7dfc2b13cd56347d8c1d9ad9b304953baa40b682
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2024-08-01 12:24:51 +0530

    port added to websockets 8

--- websocket/chat.js
+++ websocket/chat.js
@@ -4,7 +4,7 @@ import WebSocket from "ws";
 import messages from "../schema/messages.js";
 
 export const app = express();
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const httpServer = app.listen(port);
 export const wss = new WebSocketServer({ server: httpServer });
 
@@ -14,27 +14,30 @@ wss.on("connection", async function connection(ws, req) {
   console.log("Client connected");
   const urlParams = new URLSearchParams(req.url.split("?")[1]);
   const senderId = urlParams.get("senderId");
-  console.log("sender Id: " + senderId);
+  console.log(`sender Id:`, senderId);
   map.set(senderId, ws);
 
   ws.on("message", async function message(data, isBinary) {
     const { senderId, receiverId, message } = JSON.parse(data);
     let response;
 
-    console.log("Data: " + Object.keys(JSON.parse(data)).length);
-    if(Object.keys(JSON.parse(data)).length===3)
+    console.log("Data: " + message);
+    if (Object.keys(JSON.parse(data)).length === 3)
       response = await messages.create(JSON.parse(data));
-    //console.log(senderId + " " + receiverId+" "+map.has(receiverId));
+    console.log(senderId + " " + receiverId + " " + map.has(receiverId));
+    console.log(map.size);
     if (map.has(receiverId)) {
       const socket = map.get(receiverId);
-      //console.log("Socket: " + (socket.readyState === WebSocket.OPEN));
+      console.log("Socket: " + (socket.readyState === WebSocket.OPEN));
       if (socket.readyState === WebSocket.OPEN) {
-        //console.log("Socket: sent" + receiverId);
+        console.log("Socket: sent" + receiverId);
         socket.send(JSON.stringify(response), { binary: isBinary });
       }
     }
 
     // wss.clients.forEach(function each(client) {
+    //   console.log(map.has(receiverId));
+    //   console.log(map.has(senderId));
     //   if (client.readyState === WebSocket.OPEN) {
     //     client.send(data, { binary: isBinary });
     //   }
